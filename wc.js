68f732ef5070d9ed87ec37058d875295e32af55a
Santiago Mena <santimenasa@gmail.com>
2023-02-01 23:23:39 -0300

    change icon classes

--- wc.js
+++ wc.js
@@ -39,66 +39,54 @@ function descFunc() {
       if (data.weather[0].description == "clear sky") {
         descrip.textContent = "Descripcion: Soleado";
         icono.innerHTML = '<span><i class="fa-solid fa-sun"></i> </span>';
-        icono.classList.add("sunColor");
       }
       if (data.weather[0].description == "broken clouds") {
         descrip.textContent = "Descripcion: Parcialmente Nublado";
         icono.innerHTML = '<span> <i class="fa-solid fa-cloud"></i></span>';
-        icono.classList.add("cloudColor");
       }
       if (data.weather[0].description == "overcast clouds") {
         descrip.textContent = "Descripcion: Nublado";
         icono.innerHTML = '<span> <i class="fa-solid fa-cloud"></i></span>';
-        icono.classList.add("cloudColor");
       }
       if (data.weather[0].description == "few clouds") {
         descrip.textContent = "Descripcion: Poco Nublado";
         icono.innerHTML = '<span> <i class="fa-solid fa-cloud"></i> </span>';
-        icono.classList.add("sunCloud");
       }
       if (data.weather[0].description == "light rain") {
         descrip.textContent = "Descripcion: Poco Lluvioso";
         icono.innerHTML =
           '<span> <i class="fa-solid fa-cloud-rain"></i></span>';
-        icono.classList.add("cloudColor");
       }
       if (data.weather[0].description == "moderate rain") {
         descrip.textContent = "Descripcion: Lluvia Moderada";
         icono.innerHTML =
           '<span> <i class="fa-solid fa-cloud-hail"></i> </span>';
-        icono.classList.add("cloudColor");
       }
       if (data.weather[0].description == "scattered clouds") {
         descrip.textContent = "Descripcion: Nubes Dispersas";
         icono.innerHTML = '<span><i class="fa-solid fa-cloud-sun"></i></span>';
-        icono.classList.add("sunCloud");
       }
       if (data.weather[0].description == "overcast clouds") {
         descrip.textContent = "Descripcion: Mayormente Nublado";
         icono.innerHTML = '<span> <i class="fa-solid fa-cloud"></i></span>';
-        icono.classList.add("cloudColor");
       }
       if (data.weather[0].description == "thunderstorm") {
         descrip.textContent = "Descripcion: Tormenta";
         icono.innerHTML =
           '<span> <i class="fa-solid fa-cloud-showers-heavy"></i></span>';
-        icono.classList.add("cloudColor");
       }
       if (data.weather[0].description == "light snow") {
         descrip.textContent = "Descripcion: Nieve Ligera";
         icono.innerHTML =
           '<span> <i class="fa-solid fa-snowflake"></i> </span>';
-        icono.classList.add("cloudColor");
       }
       if (data.weather[0].description == "fog") {
         descrip.textContent = "Descripcion: Niebla";
         icono.innerHTML = '<span> <i class="fa-solid fa-smog"></i> </span>';
-        icono.classList.add("cloudColor");
       }
       if (data.weather[0].description == "mist") {
         descrip.textContent = "Descripcion: Neblina";
         icono.innerHTML = '<span> <i class="fa-solid fa-smog"></i> </span>';
-        icono.classList.add("cloudColor");
       }
     }
   );
